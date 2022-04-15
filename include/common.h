9f3c567a1996e3c84fd681139ed9782d64c3458f
Moonsik Park <mooonsik.park@gmail.com>
2022-04-15 16:55:10 +0900

    Add a new Thread safe queue implementation.

--- include/common.h
+++ include/common.h
@@ -11,6 +11,9 @@
 #include <string>
 #include <cstring>
 #include <stdexcept>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
 
 #include <encode.h>
 #include <encode_text.h>
@@ -34,5 +37,35 @@ typedef struct
 {
     uint32_t filesize;
 } __attribute__((packed)) RequestResponse;
+
+// from https://stackoverflow.com/questions/49640527/two-waiting-threads-producer-consumer-with-a-shared-buffer/49669316#49669316
+template <class T>
+class ThreadSafeQueue
+{
+    std::condition_variable consumer_;
+    std::mutex mutex_;
+    using unique_lock = std::unique_lock<std::mutex>;
+
+    std::queue<T> queue_;
+
+public:
+    template <class U>
+    void push_back(U &&item)
+    {
+        unique_lock lock(mutex_);
+        queue_.push(std::forward<U>(item));
+        consumer_.notify_all();
+    }
+
+    T pop_front()
+    {
+        unique_lock lock(mutex_);
+        while (queue_.empty())
+            consumer_.wait(lock);
+        auto item = queue_.front();
+        queue_.pop();
+        return item;
+    }
+};
 
 #endif // _COMMON_H_
