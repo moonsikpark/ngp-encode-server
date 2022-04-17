291cc2ea2b04f7b9011b1c73db73e0d46b340288
Moonsik Park <mooonsik.park@gmail.com>
2022-04-17 23:55:59 +0900

    add missing semicolon.

--- include/server.h
+++ include/server.h
@@ -15,6 +15,6 @@
 void socket_main_thread(std::string socket_location, ThreadSafeQueue<Request> &req_queue, ThreadSafeQueue<RenderedFrame> &frame_queue, bool threads_stop_running);
 void socket_client_thread(int clientfd, ThreadSafeQueue<Request> &req_queue, ThreadSafeQueue<RenderedFrame> &frame_queue, bool threads_stop_running);
 int socket_send_blocking(int clientfd, uint8_t *buf, ssize_t size);
-int socket_receive_blocking(int clientfd, uint8_t *buf, ssize_t size)
+int socket_receive_blocking(int clientfd, uint8_t *buf, ssize_t size);
 
 #endif // _SERVER_H_
