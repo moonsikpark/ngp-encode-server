5fa40e6afd04dc97a6c529a6fb7b8912a83dc60b
Moonsik Park <mooonsik.park@gmail.com>
2022-04-20 22:52:41 +0900

    Create POV class along with POVManager.

--- include/pov.h
+++ include/pov.h
@@ -13,6 +13,42 @@
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
-void pov_websocket_thread();
+class POV
+{
+public:
+    float x;
+    float y;
+    float z;
+    float rotx;
+    float roty;
+
+    POV() : x(0.f), y(0.f), z(0.f), rotx(0.f), roty(0.f) {}
+    POV(float x, float y, float z, float rotx, float roty) : x(x), y(y), z(z), rotx(rotx), roty(roty) {}
+};
+
+class POVManager
+{
+private:
+    POV _pov;
+    std::condition_variable _wait;
+    std::mutex _mutex;
+
+    using unique_lock = std::unique_lock<std::mutex>;
+
+public:
+    void set_pov(POV &&pov)
+    {
+        unique_lock lock(this->_mutex);
+        // XXX: Is it okay to not wait for the conditional_variable??
+        this->_pov = std::move(pov);
+    }
+
+    POV get_pov()
+    {
+        // POV data will be copy-assigned to the caller.
+        unique_lock lock(this->_mutex);
+        return this->_pov;
+    }
+};
 
 #endif // _POV_H_
