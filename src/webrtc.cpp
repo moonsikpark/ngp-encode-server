6f65666a69f27b06b6d72a33132f19b03757a53a
Moonsik Park <mooonsik.park@gmail.com>
2022-04-28 16:41:18 +0900

    Remove webrtc.{cpp|h}

--- src/webrtc.cpp
+++
@@ -1,8 +0,0 @@
-/**
- *  Copyright (c) Moonsik Park. All rights reserved.
- *
- *  @file   webrtc.cpp
- *  @author Moonsik Park, Korea Institute of Science and Technology
- **/
-
-#include <common.h>
